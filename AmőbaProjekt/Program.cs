6e3635edfad04ec6299af3558c2aee34503a5904
Dunka <pappd1@kkszki.hu>
2025-09-25 11:24:35 +0200

    Alapadatok

--- AmőbaProjekt/Program.cs
+++ AmőbaProjekt/Program.cs
@@ -10,9 +10,16 @@ namespace AmőbaProjekt
     {
         static void Main(string[] args)
         {
+            //Alapadatok
             int sorok = 10;
             int oszlopok = 10;
             string[,] tabla = new string[sorok, oszlopok];
+            string jatekos1 = "X";
+            string jatekos2 = "O";
+            string[,] hasznaltmezok = new string[sorok, oszlopok];
+            int lepesek = 0;
+            bool jatekos1fordulo = true;
+            string mostanijatekos = "1. Játékos";
             //Amőba tábla kirajzolása
             for (int i = 0; i < sorok; i++)
             {
@@ -22,9 +29,13 @@ namespace AmőbaProjekt
                     Console.Write(tabla[i, j] + " ");
                 }
                 Console.WriteLine();
-                Console.ReadLine();
             }
-            //valami dolog
+            //Játék kezdése
+            Console.WriteLine($"{mostanijatekos} következik");
+            Console.WriteLine("Add meg a sor számát");
+            int sor = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add meg az oszlop számát");
+            int oszlop = Convert.ToInt32(Console.ReadLine());
         }
     }
 }

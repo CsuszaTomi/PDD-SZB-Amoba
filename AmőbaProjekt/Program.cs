9ec8145f7fb7bee05b3770f9569bb708369ea15e
Dunka <pappd1@kkszki.hu>
2025-09-25 11:37:55 +0200

    Alap játékos inputok

--- AmőbaProjekt/Program.cs
+++ AmőbaProjekt/Program.cs
@@ -20,22 +20,50 @@ namespace AmőbaProjekt
             int lepesek = 0;
             bool jatekos1fordulo = true;
             string mostanijatekos = "1. Játékos";
-            //Amőba tábla kirajzolása
             for (int i = 0; i < sorok; i++)
             {
                 for (int j = 0; j < oszlopok; j++)
                 {
                     tabla[i, j] = ".";
+                }
+            }
+            Tábla(sorok, oszlopok, tabla);
+            do
+            {
+                //Játék kezdése
+                Console.WriteLine($"{mostanijatekos} következik");
+                Console.WriteLine("Add meg a sor számát(1-10)");
+                int sor = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Add meg az oszlop számát(1-10)");
+                int oszlop = Convert.ToInt32(Console.ReadLine());
+                if (jatekos1fordulo)
+                {
+                    tabla[sor - 1, oszlop - 1] = jatekos1;
+                    hasznaltmezok[sor - 1, oszlop - 1] = jatekos1;
+                    jatekos1fordulo = false;
+                    mostanijatekos = "2. Játékos";
+                }
+                else
+                {
+                    tabla[sor - 1, oszlop - 1] = jatekos2;
+                    hasznaltmezok[sor - 1, oszlop - 1] = jatekos2;
+                    jatekos1fordulo = true;
+                    mostanijatekos = "1. Játékos";
+                }
+                Tábla(sorok, oszlopok, tabla);
+            } while (true);
+        }
+        private static void Tábla(int sorok, int oszlopok, string[,] tabla)
+        {
+            //Amőba tábla kirajzolása
+            for (int i = 0; i < sorok; i++)
+            {
+                for (int j = 0; j < oszlopok; j++)
+                {
                     Console.Write(tabla[i, j] + " ");
                 }
                 Console.WriteLine();
             }
-            //Játék kezdése
-            Console.WriteLine($"{mostanijatekos} következik");
-            Console.WriteLine("Add meg a sor számát");
-            int sor = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Add meg az oszlop számát");
-            int oszlop = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
